2f850f576de6c919ed956d2d168025cc042a18b7
KevinMAnderson <kma2k4@hotmail.com>
2018-12-09 12:49:32 -0700

    [wip] misc

---
+++ src/AlbumArtOne/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+export default function index() {
+    return (
+        <main>
+            <article className="circle small-circle blue" />
+            <article className="circle medium-circle orange" />
+            <article>boy</article>
+            <article className="triangle small-orange" />
+            <article className="triangle large-red" />
+            <article className="triangle medium-purple" />
+        </main>
+    );
+}
