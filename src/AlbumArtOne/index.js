5c26746a1812bb82ec1a5115dcd61af7c29f23af
KevinMAnderson <kma2k4@hotmail.com>
2018-12-13 22:16:26 -0700

    added svg textured background and gradients to circles and a few lines on blue circle

--- src/AlbumArtOne/index.js
+++ src/AlbumArtOne/index.js
@@ -4,7 +4,11 @@ import "./style.css";
 export default function index() {
     return (
         <main>
-            <article className="circle small-circle blue" />
+            <article className="circle small-circle blue">
+                <p className="blueLine1" />
+                <p className="blueLine2" />
+                <p className="blueLine3" />
+            </article>
             <article className="circle medium-circle orange" />
             <article className="flower">
                 <img
