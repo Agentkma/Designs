7f527761d36206bc65008374be88dce7b284749f
KevinMAnderson <kma2k4@hotmail.com>
2018-12-14 21:34:51 -0700

    adding animations

--- src/AlbumArtOne/index.js
+++ src/AlbumArtOne/index.js
@@ -4,26 +4,30 @@ import "./style.css";
 export default function index() {
     return (
         <main>
-            <article className="circle small-circle blue">
+            <article className="circle small-circle blue fade-in-tl">
                 <p className="blueLine1" />
                 <p className="blueLine2" />
                 <p className="blueLine3" />
             </article>
-            <article className="circle medium-circle orange">
+            <article className="circle medium-circle orange slide-in-blurred-tr">
                 <p className="orangeLine1" />
                 <p className="orangeLine2" />
                 <p className="orangeLine3" />
             </article>
-            <article className="flower">
+            <article className="flower ">
                 <img
-                    className="responsive-img"
+                    className="responsive-img fade-in"
                     src={require("../assets/flower.svg")}
                     alt="flower"
                 />
             </article>
-            <article className="triangle small-orange" />
-            <article className="triangle large-red" />
-            <article className="triangle medium-purple" />
+            <article className="triangle small-orange roll-in-left" />
+            <article className="triangle large-red slide-in-blurred-bottom">
+                <p className="redLine1" />
+            </article>
+            <article className="triangle medium-purple roll-in-blurred-right ">
+                <p className="redLine1" />
+            </article>
         </main>
     );
 }
